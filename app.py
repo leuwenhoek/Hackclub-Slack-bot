4d8c664b10acbe347a1caa1895fbc4068cd8feb4
leuwenhoek <ninjabeastyy24@gmail.com>
2026-06-09 14:27:52 +0530

    UPDATED: load_dotenv function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from slack_bolt import App
 from dotenv import load_dotenv
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-load_dotenv()
+load_dotenv('.env')
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
